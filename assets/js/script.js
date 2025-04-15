8acf49f77276a8fb6b066d5e4e63b403a0122bf6
ruxi18 <ruxi.anghel@yahoo.com>
2025-04-15 17:36:50 +0300

    NEW VERSION

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.getElementById("navbar");
-  const placeholder = document.getElementById("navbar-placeholder")
-  const main = document.querySelector("main");
-  let threshold = navbar.getBoundingClientRect().top;
+// we make sure the JavaScript file loads after our HTML by using a function to test if the HTML is loaded
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}   
+
+docReady(function() {
+  const nav = document.querySelector(".navigation");
+  const offset = nav.offsetTop;
 
   window.addEventListener("scroll", function () {
-      if (window.scrollY > threshold + 300) {
-          navbar.classList.add("scrolled");
-      } else {
-          navbar.classList.remove("scrolled");
-      }
+    if (window.scrollY > offset + 50) {
+      nav.classList.add("sticky");
+    } else {
+      nav.classList.remove("sticky");
+    }
   });
 });
+
